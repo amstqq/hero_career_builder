d34d8837085f10a93bd6c4b074623fbb315a3f0c
Jackie Dong <dongjackie1999@gmail.com>
2018-10-20 16:44:08 -0700

    changes

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ app.config['JSON_SORT_KEYS'] = False
 
 Base = automap_base()
 # engine = create_engine(f"mysql://{username}:{password}@{host}/{database}")
-engine = create_engine(dbconfig)
+engine = create_engine(dbconfig, pool_recycle=1)
 Base.prepare(engine, reflect=True)
-conn = engine.connect()
+# conn = engine.connect()
 session = Session(bind=engine)
 
 DataScience = Base.classes.data_science_companies
@@ -226,4 +226,4 @@ def get_json(table):
 #################################################
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0' port=port)
